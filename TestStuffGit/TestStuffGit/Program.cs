f1e31adc037a9631fa12fb5bee2495bef0f8c022
Jurij Kuzmin <kuzmin@cngroup.dk>
2017-05-24 17:16:06 +0200

    First Commit xO

---
+++ TestStuffGit/TestStuffGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestStuffGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
